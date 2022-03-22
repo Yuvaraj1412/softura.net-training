45dd4f59cf218cbb9be18bd961e0e4045a36e64e
Yuvaraj1412 <74362547+Yuvaraj1412@users.noreply.github.com>
2022-03-22 16:13:28 +0530

    Add files via upload

---
+++ day12task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace ConsoleApp3 
+{
+    class day12task1 
+    {
+        public static void Main()
+        {
+            FileStream fs = new FileStream("D:\\softura training\\Task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            Queue q = new Queue();
+            q.Enqueue("Laddu");
+            q.Enqueue("Almond and date cake");
+            q.Enqueue("Brownies");
+            q.Enqueue("Chocolate chip cookies");
+            foreach (string cal in q)
+            {
+                sw.Write("\n" + cal);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("D:\\softura training\\Task1.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            string str = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(str);
+            FileInfo fi = new FileInfo(@"D:\softura training\Task1.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ day12task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp3
+{
+    [Serializable]
+
+    class Employee
+    {
+        public string Name = "YUVARAJ";
+        public string ID = "EM4654";
+        public string Gender = "Male";
+        public string Age = "21";
+    }
+    class day12task2
+    {
+        public void Empdata()
+        {
+            Employee emobj = new Employee();
+            FileStream fs = new FileStream("Employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, emobj);
+            fs.Close();
+        }
+        public void DeserialData()
+        {
+            FileStream fs = new FileStream("Employee.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            Employee emobj = (Employee)f.Deserialize(fs);
+            Console.WriteLine(emobj.Name);
+            Console.WriteLine(emobj.ID);
+            Console.WriteLine(emobj.Gender);
+            Console.WriteLine(emobj.Age);
+        }
+
+        public static void Main()
+        {
+            day12task2 obj = new day12task2();
+            obj.Empdata();
+            obj.DeserialData();
+            Console.Read();
+        }
+    }
+}
