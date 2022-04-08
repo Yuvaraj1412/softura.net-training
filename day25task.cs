1c6fc96ad66b5372f1524ab554b77b1297539d43
Yuvaraj1412 <74362547+Yuvaraj1412@users.noreply.github.com>
2022-04-08 21:04:51 +0530

    Add files via upload

---
+++ day25task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq
+{
+    class day25task
+    {
+		static int countGoat(int eyes,int Legs)
+		{
+			int count = 0;
+			count = (Legs) - (eyes - 1);
+			count = count / 2;
+			return count;
+		}
+		public static void Main()
+		{
+			int eyes = 52, Legs = 80;
+			int Goat = countGoat(eyes, Legs);
+			Console.WriteLine("Goat = " + Goat);
+			Console.WriteLine("Ducks = " + (Goat - 2));
+		}
+	}
+}
+
